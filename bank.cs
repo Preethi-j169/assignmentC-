17e7f7d11dffb8925adc5a8515a75023eac1efc4
PREETHI <preethi.j@sonata-software.com>
2021-09-30 23:17:22 +0530

    day 02 assignment

---
+++ bank.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_Csharp
+{
+    class bank
+    {
+        int min_balance, balance1;
+        public bank(int balance1)
+        {
+            min_balance = 500;
+            this.balance1 = balance1;
+        }
+        public static void Main ()
+        {
+            bank balance = new bank(501);
+            Console.WriteLine("default_balance=" + balance.min_balance);
+            Console.WriteLine("new_balance=" + balance.balance1);
+        }
+    }
+
+}
+
---
+++ employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_Csharp
+{
+    class employee
+    {
+        public static void Main()
+        {
+            Double Basic_Salary, HRA, Travel_Allowance;
+
+            Console.Write("Enter Basic Salary : ");
+            Basic_Salary = Convert.ToInt32(Console.ReadLine());
+
+           
+            HRA = (Basic_Salary * 20) / 100;
+            Travel_Allowance = Basic_Salary  + HRA;
+
+           
+            Console.Write("\nHR ALLOWANCE : " + HRA);
+            Console.Write("\nTRAVEL ALLOWANCE : " + Travel_Allowance);
+        }
+    }
+}
