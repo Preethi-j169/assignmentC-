6dae45b419f7e131389fcad8fa8c0991ef0e6e5a
Preethi-j169 <90926755+Preethi-j169@users.noreply.github.com>
2021-10-01 09:03:19 +0530

    Update employee.cs

--- employee.cs
+++ employee.cs
@@ -8,20 +8,60 @@ namespace assignment_Csharp
 {
     class employee
     {
-        public static void Main()
+       string name;
+        int empid;
+        int basic;
+        int hr;
+        int trvlall;
+
+        public EmployeeSalary(string name,int empid,int basic,int hr,int trvlall)
         {
-            Double Basic_Salary, HRA, Travel_Allowance;
+            this.name = name;
+            this.empid = empid;
+            this.basic = basic;
+            this.hr = hr;
+            this.trvlall = trvlall;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public int getEmpid()
+        {
+            return empid;
+        }
+        public int getBasic()
+        {
+            return basic;
+        }
+        public int getHr()
+        {
+            return basic;
+        }
+        public int getTrvl()
+        {
+            return basic;
+        }
+        public double tax()
+        {
+            if (basic * 12 < 50000 && basic * 12 > 0)
+                return basic * 0;
+            else if (basic * 12 > 50000 && basic * 12 < 70000)
+                return basic * .15;
+            else
+                return basic * .2;
+        }
+        public int gross()
+        {
+            return basic + hr + trvlall;
+        }
+        static void Main(string[] args)
+        {
+            EmployeeSalary emp = new EmployeeSalary("Chirag", 20792, 6000, 1000, 500);
 
-            Console.Write("Enter Basic Salary : ");
-            Basic_Salary = Convert.ToInt32(Console.ReadLine());
-
-           
-            HRA = (Basic_Salary * 20) / 100;
-            Travel_Allowance = Basic_Salary  + HRA;
-
-           
-            Console.Write("\nHR ALLOWANCE : " + HRA);
-            Console.Write("\nTRAVEL ALLOWANCE : " + Travel_Allowance);
+            Console.WriteLine("Hello"+emp.getName()+".\n Emp ID:"+emp.getEmpid()+".\n Basic salary:"+ emp.getBasic()+".\n HR Allowance:"+emp.getHr()+ ".\n Travel Allowance:" + emp.getTrvl()++".\n Tax:" + emp.tax()+".\n Gross"+emp.gross());
         }
     }
 }
+
+ 
